7f5673f1f654c92144f77b4e1e6170082b222a98
Mounika Harini <146058701+Mounikaharini@users.noreply.github.com>
2026-06-11 18:58:04 +0530

    Create controller.java

---
+++ controller.java
@@ -0,0 +1,19 @@
+package com.day2.project2;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class controller {
+	
+//	@GetMapping("/task1ForGetMapping")
+//	public String getData() {
+//		return "hi";
+//	}
+	
+//	@GetMapping("task2ForGetMapping")
+//	public String getData1() {
+//		String op ="<html><head><title>document</title></head><body><h1>Date : 11.06.2026</h1></body></html>";
+//		return op;
+//	}
+}
